fe29f71de02f56bbf8a23af436cf49dfc804e2c3
jipara <zhypargul.esengulova@gmail.com>
2021-03-29 20:29:45 +0000

    add cluster

---
+++ gke-cluster-output.tf
@@ -0,0 +1,8 @@
+  
+output "cluster-name" {
+  value = google_container_cluster.gke-cluster.name
+}
+
+output "cluster-location" {
+  value = google_container_cluster.gke-cluster.location
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -3,4 +3,4 @@ gcp_region_1 = "us-central1"
 gcp_zone_1   = "us-central1-b"
 
 # Application Name (used in resource names, no spaces.)
-app_name = "test"
+app_name = ""kubernetes-bootcamp
